6e48e6f1821995713617eda0012b114da01ac0cc
alexandr <sanya-arsenal@mail.ru>
2020-11-29 23:06:54 +0300

    Add the back button click

--- app/src/main/java/ru/uniquenature/myapplication/FragmentMoviesDetails.kt
+++ app/src/main/java/ru/uniquenature/myapplication/FragmentMoviesDetails.kt
@@ -4,16 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 import androidx.fragment.app.Fragment
 
 class FragmentMoviesDetails : Fragment() {
+    private var backFragment: TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View?= inflater.inflate(R.layout.fragment_movies_details,container,false)
+    ): View? {
+       val view = inflater.inflate(R.layout.fragment_movies_details,container,false)
+        backFragment = view.findViewById(R.id.tv_back)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        backFragment?.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container,FragmentMoviesList()).commit()
+        }
+    }
 
 }
 
--- app/src/main/java/ru/uniquenature/myapplication/FragmentMoviesList.kt
+++ app/src/main/java/ru/uniquenature/myapplication/FragmentMoviesList.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 
@@ -24,11 +23,9 @@ class FragmentMoviesList : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         clReplaceFragment?.setOnClickListener {
-            activity?.let {
                 requireActivity().supportFragmentManager.beginTransaction()
                     .replace(R.id.main_container,FragmentMoviesDetails())
                     .addToBackStack("FragmentMoviesDetails").commit()
-            }
         }
     }
 
